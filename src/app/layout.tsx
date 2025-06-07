03dd80cd69f7c55bea32e58bdcc8b8fd41e4e1ea
kerkeniaziz <kerkeniaziz@gmail.com>
2025-06-07 01:38:19 +0100

    update sitemap last modified date and add development notice in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,23 +25,30 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-VEXX4J9MEZ"></script>
-      {/* eslint-disable-next-line @next/next/no-before-interactive-script-outside-document */}
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-VEXX4J9MEZ"></script>
+        {/* eslint-disable-next-line @next/next/no-before-interactive-script-outside-document */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
         window.dataLayer = window.dataLayer || [];
         function gtag(){window.dataLayer.push(arguments);}
         gtag('js', new Date());
         gtag('config', 'G-VEXX4J9MEZ');
           `,
-        }}
-      />
+          }}
+        />
       </head>
-      
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div className="fixed top-18 z-150 ">
+          <div className="bg-primary text-white text-lg font-bold px-4 py-4 shadow-lg hover:bg-primary/60 rounded-br-full rounded-tr-full ">
+            Still in Development
+          </div>
+        </div>
+
+
         {children}
       </body>
     </html>
